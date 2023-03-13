1980247aa3e3fb7f6c8bb52f3000cea3dc7650f1
Luk Gutierrez <lucianogutierrezagustin@gmail.com>
2023-03-13 18:31:58 -0300

    version_beta_1.0

--- lib/pages/Home_Page.dart
+++ lib/pages/Home_Page.dart
@@ -12,7 +12,7 @@ class HomePage extends StatefulWidget {
 }
 
 Future<String> generateResponse(String prompt) async {
-  const apiKey = "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";
+  const apiKey = "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";
 
   var url = Uri.https("api.openai.com", "/v1/completions");
   final response = await http.post(
@@ -36,13 +36,90 @@ Future<String> generateResponse(String prompt) async {
 
   return newresponse['choices'][0]['text'];
 }
-  ListView _buildList() {
+
+class _HomePageState extends State<HomePage> {
+  final _textController = TextEditingController();
+  final _scrollController = ScrollController();
+  final List<ChatMessage> _messages = [];
+  late bool isLoading;
+
+  @override
+  void initState() {
+    super.initState();
+    isLoading = false;
+  }
+
+  Widget _buildSubmit() {
+    return Visibility(
+      visible: !isLoading,
+      child: Container(
+        child: IconButton(
+          icon: const Icon(
+            Icons.send_rounded,
+            color: Color.fromRGBO(142, 142, 160, 1),
+          ),
+          onPressed: () async {
+            setState(
+              () {
+                _messages.add(
+                  ChatMessage(
+                    text: _textController.text,
+                    chatMessageType: ChatMessageType.user,
+                  ),
+                );
+                isLoading = true;
+              },
+            );
+            var input = _textController.text;
+            _textController.clear();
+            Future.delayed(const Duration(milliseconds: 50))
+                .then((_) => _scrollDown());
+            generateResponse(input).then((value) {
+              setState(() {
+                isLoading = false;
+                _messages.add(
+                  ChatMessage(
+                    text: value,
+                    chatMessageType: ChatMessageType.bot,
+                  ),
+                );
+              });
+            });
+            _textController.clear();
+            Future.delayed(const Duration(milliseconds: 50))
+                .then((_) => _scrollDown());
+          },
+        ),
+      ),
+    );
+  }
+
+  Expanded _buildInput() {
+    return Expanded(
+      child: TextField(
+        textCapitalization: TextCapitalization.sentences,
+        style: const TextStyle(color: Colors.white),
+        controller: _textController,
+        decoration: const InputDecoration(
+          filled: true,
+          border: InputBorder.none,
+          focusedBorder: InputBorder.none,
+          enabledBorder: InputBorder.none,
+          errorBorder: InputBorder.none,
+          disabledBorder: InputBorder.none,
+        ),
+      ),
+    );
+  }
+
+  ///////////////LIST/////////////////
+  _buildList() {
     return ListView.builder(
       controller: _scrollController,
       itemCount: _messages.length,
       itemBuilder: (context, index) {
         var message = _messages[index];
-        return Chatmessage(
+        return Chatmessagess(
           text: message.text,
           chatMessageType: message.chatMessageType,
         );
@@ -50,37 +127,47 @@ Future<String> generateResponse(String prompt) async {
     );
   }
 
+////////////////////////////////////////////////////
+  void _scrollDown() {
+    _scrollController.animateTo(
+      _scrollController.position.maxScrollExtent,
+      duration: const Duration(milliseconds: 300),
+      curve: Curves.easeOut,
+    );
+  }
+/////////////////////////////////////////
 
-class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("PraticeOPENIA"),
+        backgroundColor: Colors.black,
+        title: Text("@Lukty Intellenge Artificial"),
       ),
-      body: Column(
-        children: [
-          
-        ],
-      ),
+      body: Column(children: [
+        Expanded(child: _buildList()),
+        Row(
+          children: [_buildInput(), _buildSubmit()],
+        )
+      ] //[_buildList()],
+          ),
     );
   }
 }
 
+///PRACTICE////////////////////
 
-class Chatmessage extends StatelessWidget {
+class Chatmessagess extends StatelessWidget {
   final String text;
   final ChatMessageType chatMessageType;
-  const Chatmessage({required this.chatMessageType,required this.text})
-  
+  const Chatmessagess({required this.chatMessageType, required this.text});
 
   @override
   Widget build(BuildContext context) {
     return Container(
-       
       child: Row(
         crossAxisAlignment: CrossAxisAlignment.start,
-        children:[
+        children: [
           chatMessageType == ChatMessageType.bot
               ? Container(
                   child: Icon(Icons.access_alarm_outlined),
@@ -104,7 +191,6 @@ class Chatmessage extends StatelessWidget {
                   ),
                   child: Text(
                     text,
-                    
                   ),
                 ),
               ],
